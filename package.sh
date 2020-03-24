fa80f43126ff97248c917777ea71064caa31ebe7
Kane York <rikingcoding@gmail.com>
2020-03-24 12:01:21 -0700

    Fix release script to include .exe

--- package.sh
+++ package.sh
@@ -14,7 +14,7 @@ rm -r build/go-redirect-agent
 mkdir build/go-redirect-agent
 
 echo "build windows.amd64"
-GOOS=windows GOARCH=amd64 go build -o build/go-redirect-agent/go-redirect-agent .
+GOOS=windows GOARCH=amd64 go build -o build/go-redirect-agent/go-redirect-agent.exe .
 cp hosts_windows.bat build/go-redirect-agent/hosts_windows.bat
 (cd build; zip -r ../target/go-redirect-agent.windows.amd64.zip go-redirect-agent/ )
 
